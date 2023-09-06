c6b5da5b2d3a6b76b50511d53f41f433649be91f
Rocio Dure <rocioevelyndure@gmail.com>
2023-09-06 15:15:36 -0300

    cambios en servicios Base

--- modulos/base/servicios.py
+++ modulos/base/servicios.py
@@ -4,15 +4,21 @@ from ..base.modelo import ModeloBase
 from typing import List
 
 class ServiciosBase(ABC):
-    def item_a_fila(self):
-        #objeto (tipo modelo base a fila )
-        print("p")
+    def item_a_fila(self, item:ModeloBase)->dict:
+        data=item.__dict__
+        return data
     
+    def fila_a_item(self, fila:dict)->ModeloBase:
+        #recibe un dict y lo transforma en una instancia de la clase ModeloBase 
+         item=ModeloBase()
+         item.id=fila['id']
+         return item
+         
     
-    def fila_a_item(self):
-        #
+    def fila_a_sql(self,fila:dict):
+        #recibe un dict () y lo transforma en una cadena sql (str)
          print("p")
-    
-    def fila_a_sql(self):
-        #
-         print("p")
+         sql=str(fila)
+         return sql
+         
+         
